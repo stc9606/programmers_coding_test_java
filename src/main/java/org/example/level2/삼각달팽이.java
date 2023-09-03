a5b67fe863d605b440649aea0e4980bfb6a84418
scnoh <stc9606@naver.com>
2023-09-03 19:50:33 +0900

    level2. 삼각 달팽이 - 코드 개선

--- src/main/java/org/example/level2/삼각달팽이.java
+++ src/main/java/org/example/level2/삼각달팽이.java
@@ -2,7 +2,9 @@ package org.example.level2;
 
 public class 삼각달팽이 {
 
-    private static int N = 4;
+    private static final int N = 4;
+    private static final int[] dx = { 0, 1, -1 };
+    private static final int[] dy = { 1, 0, -1 };
 
     public static void main(String[] args) {
         // 1. 2차원 배열 선언
@@ -12,45 +14,21 @@ public class 삼각달팽이 {
         // 2. 숫자를 채운 현재를 (0,0)으로 설정
         int x = 0;
         int y = 0;
+        int d = 0;
 
         while (true) {
-            // (1) 아래로 이동
-            while (true) {
-                triangle[y][x] = v++;
-                if (y + 1 == N || triangle[y + 1][x] != 0)
+            triangle[y][x] = v++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (nx == N || ny == N || nx == -1 || ny == -1 || triangle[ny][nx] != 0) {
+                d = (d + 1) % 3;
+                nx = x + dx[d];
+                ny = y + dy[d];
+                if (nx == N || ny == N || nx == -1 || ny == -1 || triangle[ny][nx] != 0)
                     break;
-                y += 1;
             }
-
-            if (x + 1 == N || triangle[y][x + 1] != 0)
-                break;
-            x += 1;
-
-            // (2) 오른쪽으로 이동
-            while (true) {
-                triangle[y][x] = v++;
-                if (x + 1 == N || triangle[y][x + 1] != 0)
-                    break;
-                x += 1;
-            }
-
-            if (triangle[y -1][x - 1] != 0)
-                break;
-            x -= 1;
-            y -= 1;
-
-            // (3) 왼쪽 위로 이동
-            while (true) {
-                triangle[y][x] = v++;
-                if (triangle[y - 1][x - 1] != 0)
-                    break;
-                x -= 1;
-                y -= 1;
-            }
-
-            if (y + 1 == N || triangle[y + 1][x] != 0)
-                break;
-            y += 1;
+            x = nx;
+            y = ny;
         }
 
         int[] result = new int[v - 1];

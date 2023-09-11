50ebbca3774860a425015e8e9f1c027a0ec8dd7e
scnoh <stc9606@naver.com>
2023-09-11 22:22:13 +0900

    쿼드 압축 후 개수 세기

---
+++ src/main/java/com/programmers/recursive/쿼드압축후개수세기.java
@@ -0,0 +1,47 @@
+package com.programmers.recursive;
+
+import java.util.Arrays;
+
+public class 쿼드압축후개수세기 {
+
+    private static final int[][] arr = { {1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1} };
+
+    public static void main(String[] args) {
+        Count count = count(0, 0, arr.length, arr);
+        int[] result = {count.zero, count.one};
+
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static Count count(int offsetX, int offsetY, int size, int[][] arr) {
+        int h = size / 2;
+        for (int x = offsetX; x < offsetX + size; x++) {
+            for (int y = offsetY; y < offsetY + size; y++) {
+                if (arr[y][x] != arr[offsetY][offsetX])
+                    return count(offsetX, offsetY, h, arr)
+                            .add(count(offsetX + h, offsetY, h, arr))
+                            .add(count(offsetX, offsetY + h, h, arr))
+                            .add(count(offsetX + h, offsetY + h, h, arr));
+            }
+        }
+
+        if (arr[offsetY][offsetX] == 1) {
+            return new Count(0, 1);
+        }
+        return new Count(1, 0);
+    }
+
+    private static class Count {
+        public final int zero;
+        public final int one;
+
+        public Count(int zero, int one) {
+            this.zero = zero;
+            this.one = one;
+        }
+
+        public Count add(Count other) {
+            return new Count(zero + other.zero, one + other.one);
+        }
+    }
+}

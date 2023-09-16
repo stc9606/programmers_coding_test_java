efa118ac0fb9e7187f248195a26216056abe72fe
scnoh <stc9606@naver.com>
2023-09-16 15:30:49 +0900

    하노이의 탑 - 최적화

--- src/main/java/com/programmers/recursive/하노이의탑.java
+++ src/main/java/com/programmers/recursive/하노이의탑.java
@@ -6,24 +6,25 @@ import java.util.List;
 
 public class 하노이의탑 {
 
-    private static final int N = 2;
+    private static final int N = 15;
 
     public static void main(String[] args) {
-        int[][] result = hanoi(N, 1, 3).toArray(new int[0][]);
-        System.out.println(Arrays.deepToString(result));
+        List<int[]> process = new ArrayList<>();
+        hanoi(N, 1, 3, process);
+        System.out.println(Arrays.deepToString(process.toArray(new int[0][])));
     }
 
-    private static List<int[]> hanoi(int n, int from, int to) {
+    private static void hanoi(int n, int from, int to, List<int[]> process) {
         // 종료 조건, 점화식 구현
-        if (n == 1)
-            return List.of(new int[] { from, to });
-        int empty = 6 - from - to; // 기둥이 1,2,3 이고 to를 제외한 빈 기둥을 찾기 위한 empty를 구하는 식
+        if (n == 1) {
+            process.add(new int[] { from, to });
+            return;
+        }
 
-        List<int[]> result = new ArrayList<>();
-        result.addAll(hanoi(n-1, from, empty));
-        result.addAll(hanoi(n, from, to));
-        result.addAll(hanoi(n-1, empty, to));
-        return result;
+        int empty = 6 - from - to; // 기둥이 1,2,3 이고 to를 제외한 빈 기둥을 찾기 위한 empty를 구하는 식
+        hanoi(n-1, from, empty, process);
+        hanoi(1, from, to, process);
+        hanoi(n-1, empty, to, process);
     }
 
 }

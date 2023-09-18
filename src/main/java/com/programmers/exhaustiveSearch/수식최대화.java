2b8c06e3537ba641dcfc342e0df2d508cae4510f
scnoh <stc9606@naver.com>
2023-09-18 20:26:38 +0900

    수식 최대화

--- src/main/java/com/programmers/exhaustiveSearch/수식최대화.java
+++ src/main/java/com/programmers/exhaustiveSearch/수식최대화.java
@@ -1,10 +1,15 @@
 package com.programmers.exhaustiveSearch;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class 수식최대화 {
 
     private static final String expression = "100-200*300-500+20";
 
     private static final String[][] precedences = {
+
+
             "+-*".split(""),
             "+*-".split(""),
             "-+*".split(""),
@@ -15,7 +20,8 @@ public class 수식최대화 {
 
     public static void main(String[] args) {
 
-
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        executorService.shutdownNow();
 
     }
 

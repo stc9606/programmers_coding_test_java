e4f237a08d0168204570c5a3ae1287c4f4cb31e3
scnoh <stc9606@naver.com>
2023-09-18 21:21:59 +0900

    수식 최대화

--- src/main/java/com/programmers/exhaustiveSearch/수식최대화.java
+++ src/main/java/com/programmers/exhaustiveSearch/수식최대화.java
@@ -1,5 +1,8 @@
 package com.programmers.exhaustiveSearch;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -8,8 +11,6 @@ public class 수식최대화 {
     private static final String expression = "100-200*300-500+20";
 
     private static final String[][] precedences = {
-
-
             "+-*".split(""),
             "+*-".split(""),
             "-+*".split(""),
@@ -19,11 +20,52 @@ public class 수식최대화 {
     };
 
     public static void main(String[] args) {
+        StringTokenizer tokenizer = new StringTokenizer(expression, "+-*", true);
+        List<String> tokens = new ArrayList<>();
+        while (tokenizer.hasMoreTokens()) {
+            tokens.add(tokenizer.nextToken());
+        }
 
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.shutdownNow();
+        long max = 0;
+        for (String[] precedence : precedences) {
+            long value = Math.abs(calculate(new ArrayList<>(tokens), precedence));
+            if (value > max) {
+                max = value;
+            }
+        }
 
+        System.out.println(max);
     }
 
+    private static long calculate(long lhs, long rhs, String op) {
+        switch (op) {
+            case "+":
+                return lhs + rhs;
+            case "-":
+                return lhs - rhs;
+            case "*":
+                return lhs * rhs;
+            default:
+                return 0;
+        }
+    }
+
+    private static long calculate(List<String> tokens, String[] precedence) {
+        for (String op : precedence) {
+            for (int i = 0; i < tokens.size(); i++) {
+                if (tokens.get(i).equals(op)) {
+                    long lhs = Long.parseLong(tokens.get(i - 1));
+                    long rhs = Long.parseLong(tokens.get(i + 1));
+                    long result = calculate(lhs, rhs, op);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.remove(i - 1);
+                    tokens.add(i - 1, String.valueOf(result));
+                    i -= 2;
+                }
+            }
+        }
+        return Long.parseLong(tokens.get(0));
+    }
 
 }

1eb3e28ba6207409d1b7875f5b60ede5757debfb
scnoh <stc9606@naver.com>
2023-09-23 20:12:00 +0900

    소수 찾기 - 최적화

--- src/main/java/com/programmers/exhaustiveSearch/소수찾기.java
+++ src/main/java/com/programmers/exhaustiveSearch/소수찾기.java
@@ -11,25 +11,25 @@ public class 소수찾기 {
     private static final String number = "17";
 
     public static void main(String[] args) {
-        List<Integer> numbers = number.chars()
-                .map(c -> c - '0')
-                .boxed()
-                .collect(Collectors.toList());
+        Set<Integer> primes = new HashSet<>();
+        int[] numbers = number.chars().map(c -> c + '0').toArray();
 
-        int size = getPrimes(0, numbers).size();
-        System.out.println(size);
+        getPrimes(0, numbers, new boolean[numbers.length], primes);
+        System.out.println(primes.size());
 
     }
 
-    private static Set<Integer> getPrimes(int acc, List<Integer> numbers) {
-        Set<Integer> primes = new HashSet<>();
+    private static Set<Integer> getPrimes(int acc, int[] numbers, boolean[] isUsed, Set<Integer> primes) {
         if (isPrime(acc)) primes.add(acc);
 
-        for (int i = 0; i < numbers.size(); i++) {
-            int nextAcc = acc * 10 + numbers.get(i);
-            List<Integer> nextNumbers = new ArrayList<>(numbers);
-            nextNumbers.remove(i);
-            primes.addAll(getPrimes(nextAcc, nextNumbers));
+        for (int i = 0; i < numbers.length; i++) {
+            if (isUsed[i]) continue;
+
+            int nextAcc = acc * 10 + numbers[i];
+
+            isUsed[i] = true;
+            getPrimes(nextAcc, numbers, isUsed, primes);
+            isUsed[i] = false;
         }
 
         return primes;

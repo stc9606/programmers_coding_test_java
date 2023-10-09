00974bcceaa049758fe8a2be306cca7024ae39d0
scnoh <stc9606@naver.com>
2023-10-09 22:11:49 +0900

    소수찾기

--- src/main/java/com/programmers/exhaustiveSearch/소수찾기.java
+++ src/main/java/com/programmers/exhaustiveSearch/소수찾기.java
@@ -1,10 +1,7 @@
 package com.programmers.exhaustiveSearch;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class 소수찾기 {
 
